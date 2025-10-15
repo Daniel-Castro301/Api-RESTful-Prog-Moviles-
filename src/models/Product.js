7c25bd215d09491b4a043521ee270938789b961e
Daniel-Castro301 <dacastro002@gmail.com>
2025-10-15 12:53:58 -0500

    primer commit

---
+++ src/models/Product.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../database/connection');
+const Product = sequelize.define('Product', {
+ name: {
+ type: DataTypes.STRING,
+ allowNull: false
+ },
+ price: {
+ type: DataTypes.FLOAT,
+ allowNull: false
+ },
+ description: {
+ type: DataTypes.TEXT
+ },
+ stock: {
+ type: DataTypes.INTEGER,
+ defaultValue: 0
+ }
+}, {
+ timestamps: true // Agrega los campos createdAt y updatedAtautomáticamente
+ });
+module.exports = Product;
